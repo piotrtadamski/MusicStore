0104e71951173163c3535aa5f48c123c1f1e3904
Piotr Adamski <piotrt.adamski@gmail.com>
2022-01-20 22:36:15 +0100

    add empty controllers

---
+++ src/Infrastructure/Ports/Api/Controller/BandController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MusicStore\Infrastructure\Ports\Api\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BandController extends AbstractController
+{
+
+}
---
+++ src/Infrastructure/Ports/Api/Controller/TrackController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MusicStore\Infrastructure\Ports\Api\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class TrackController extends AbstractController
+{
+
+}
