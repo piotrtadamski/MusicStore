e2baf8b8c628bc7d06461ffd3dd6edb0c5efd55a
Piotr Adamski <piotrt.adamski@gmail.com>
2022-01-21 11:35:25 +0100

    mod dbal yeartype

--- src/Infrastructure/Adapters/Doctrine/DBAL/Types/YearType.php
+++ src/Infrastructure/Adapters/Doctrine/DBAL/Types/YearType.php
@@ -7,8 +7,20 @@ use MusicStore\Domain\Common\Types\Year;
 
 final class YearType extends AbstractStringType
 {
+    public const YEAR = 'YEAR(4)';
+
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return self::YEAR;
+    }
+
     protected function handleConvertToPHPValue(string $value, AbstractPlatform $platform)
     {
         return Year::fromString($value);
+    }
+
+    public function getName()
+    {
+        return self::YEAR;
     }
 }
