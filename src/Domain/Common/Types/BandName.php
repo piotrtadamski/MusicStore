5b7c4ea4bdc01b991d5a9312eedfb3cba97ce52c
Piotr Adamski <piotrt.adamski@gmail.com>
2022-01-21 10:23:04 +0100

    add domain bandname type

---
+++ src/Domain/Common/Types/BandName.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace MusicStore\Domain\Common\Types;
+
+class BandName
+{
+    private string $bandName;
+
+    public function __construct(string $bandName)
+    {
+        $this->bandName = $bandName;
+    }
+
+    public static function fromString(string $value)
+    {
+        return new self($value);
+    }
+
+    public function __toString()
+    {
+        return $this->bandName;
+    }
+}
