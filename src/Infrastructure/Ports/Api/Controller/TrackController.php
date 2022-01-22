486a80c2322458c2884ae7af6407695bd28cde4e
Piotr Adamski <piotrt.adamski@gmail.com>
2022-01-22 17:43:08 +0100

    expand TrackController skeleton Rest Api over PUT POST GET DELETE

--- src/Infrastructure/Ports/Api/Controller/TrackController.php
+++ src/Infrastructure/Ports/Api/Controller/TrackController.php
@@ -3,8 +3,53 @@
 namespace MusicStore\Infrastructure\Ports\Api\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route(path="/api/track")
+ */
 class TrackController extends AbstractController
 {
+    /**
+     * @Route(path="", methods={"GET"})
+     */
+    public function show()
+    {
+        return JsonResponse::create([], Response::HTTP_NOT_IMPLEMENTED);
+    }
 
+    /**
+     * @Route(path="", methods={"PUT"})
+     */
+    public function update(Request $request)
+    {
+        return JsonResponse::create([], Response::HTTP_NOT_IMPLEMENTED);
+    }
+
+    /**
+     * @Route(path="", methods={"DELETE"})
+     */
+    public function remove(Request $request)
+    {
+        return JsonResponse::create([]);
+    }
+
+    /**
+     * @Route(path="", methods={"GET"})
+     */
+    public function listAll(Request $request)
+    {
+        return JsonResponse::create([], Response::HTTP_NOT_IMPLEMENTED);
+    }
+
+    /**
+     * @Route(path="", methods={"POST"})
+     */
+    public function create()
+    {
+        return JsonResponse::create([], Response::HTTP_NOT_IMPLEMENTED);
+    }
 }
