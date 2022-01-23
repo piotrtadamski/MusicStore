ebf1f356b5eaa71e408cc45f3c7a769912830cab
Piotr Adamski <piotrt.adamski@gmail.com>
2022-01-23 21:07:24 +0100

    mod PromoteAlbum command to use subscribed users

--- src/Application/Command/Album/PromoteAlbumHandler.php
+++ src/Application/Command/Album/PromoteAlbumHandler.php
@@ -3,15 +3,25 @@
 namespace MusicStore\Application\Command\Album;
 
 use MusicStore\Application\Command\CommandHandlerInterface;
+use MusicStore\Application\Notification\MailerInterface;
 use MusicStore\Domain\Album\AlbumRepositoryInterface;
 
 class PromoteAlbumHandler implements CommandHandlerInterface
 {
+    /** @var string[] */
+    private array $recipients = [];
     private AlbumRepositoryInterface $albumRepository;
+    private MailerInterface $mailer;
 
-    public function __construct(AlbumRepositoryInterface $albumRepository)
+    public function __construct(
+        AlbumRepositoryInterface $albumRepository,
+        MailerInterface          $mailer,
+        array                    $recipients = []
+    )
     {
         $this->albumRepository = $albumRepository;
+        $this->mailer = $mailer;
+        $this->recipients = $recipients;
     }
 
     public function __invoke(PromoteAlbum $command)
